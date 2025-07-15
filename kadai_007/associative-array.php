2a35f08cbc164795c5b3ce0f8c15959db7101ef4
yo447 <kakuto1019@gmail.com>
2025-07-15 20:52:33 +0900

    commit

--- kadai_007/associative-array.php
+++
@@ -1,5 +0,0 @@
-<?php
-$array = Array('name' => 'onion', 'price' => 200, 'weight' => 160);
-
-print_r($array);
-?>
--- kadai_011/foreach.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-$syohin = ['名前' => '玉ねぎ', '値段' => 200, '産地' => '北海道'];
-
-foreach($syohin as $key => $value){
-    echo "{$key} : {$value}".'<br>';
-    
-}
-?>
