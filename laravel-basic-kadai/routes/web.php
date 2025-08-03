c134c1783647ccebdf41da36fb88b18bd3b0efa6
yo447 <kakuto1019@gmail.com>
2025-08-03 09:59:32 +0900

    commit

--- laravel-basic-kadai/routes/web.php
+++ laravel-basic-kadai/routes/web.php
@@ -1,11 +1,13 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PostController;
 use App\Http\Controllers\ProductController;
 use App\Http\Controllers\VendorController;
 use App\Http\Controllers\RequestController;
 use App\Http\Controllers\ResponseController;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -22,6 +24,18 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__.'/auth.php';
+
 Route::get('/posts', function () {
     return view('posts.index');
 });
@@ -30,11 +44,15 @@ Route::get('/posts', [PostController::class, 'index']);
 
 Route::post('/posts/confirm', [PostController::class, 'confirm'])->name('posts.confirm');
 
-Route::get('/posts/create', [PostController::class, 'create']);
+Route::get('/posts/create', [PostController::class, 'create'])->middleware('auth');
 
 //これがいらなかったRoute::get('/posts', [PostController::class, 'create']);
 
-Route::post('/posts/store', [PostController::class, 'store'])->name('posts.store');
+Route::post('/posts/store', [PostController::class, 'store'])->name('posts.store')->middleware('auth');
+
+Route::get('/vendors/{id}', [VendorController::class, 'show']);
+
+//======== 後略 ========
 
 Route::get('/posts/{id}', [PostController::class, 'show']);
 
@@ -52,11 +70,13 @@ Route::get('/products/create', [ProductController::class, 'create']);
 Route::post('/products/store', [ProductController::class, 'store'])->name('products.store');
 
 Route::get('/products/{id}', [ProductController::class, 'show']);
-Route::get('/vendors/create', [VendorController::class, 'create']);
+
+Route::get('/vendors/create', [VendorController::class, 'create'])->middleware('auth');
+
+Route::post('/vendors/store', [VendorController::class, 'store'])->name('vendors.store')->middleware('auth');
 
 Route::get('/vendors/{id}', [VendorController::class, 'show']);
 
-Route::post('/vendors/store', [VendorController::class, 'store'])->name('vendors.store');
 
 Route::get('/requests/create', [RequestController::class, 'create']);
 
